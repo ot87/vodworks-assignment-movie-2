12aa51dc1c10c4eae6accbdbada0e959389b5f14
ot87 <oleksandr.tsyrkul@gmail.com>
2021-12-30 22:11:50 +0200

    Add wrapper function for render in Genreitem tests.

--- src/components/GenreList/GenreItem/GenreItem.test.js
+++ src/components/GenreList/GenreItem/GenreItem.test.js
@@ -5,42 +5,54 @@ import { genres } from '../../../utils/testData';
 
 const genre = genres[0];
 
+const renderGenreItem = (renderProps = {}) => {
+  const initProps = { item: genres[0] };
+  const { rerender } = render(<GenreItem {...initProps} {...renderProps} />);
+
+  return {
+    genreItem: screen.getByText(genre),
+    rerenderGenreItem: rerenderProps => {
+      rerender(<GenreItem {...initProps} {...rerenderProps} />);
+    }
+  };
+};
+
 describe('GenreItem', () => {
   it('renders with name', () => {
-    render(<GenreItem item={genre} />);
-    expect(screen.getByText(genre)).toBeInTheDocument();
+    const { genreItem } = renderGenreItem();
+    expect(genreItem).toBeInTheDocument();
   });
 
   it('renders as not active and not selected', () => {
-    render(<GenreItem item={genre} />);
-    expect(screen.getByText(genre)).not.toHaveClass('genre-item-active');
-    expect(screen.getByText(genre)).not.toContainHTML('<span class="arrow"></span>');
+    const { genreItem } = renderGenreItem();
+    expect(genreItem).not.toHaveClass('genre-item-active');
+    expect(genreItem).not.toContainHTML('<span class="arrow"></span>');
   });
 
   it('renders and rerenders as selected and back', () => {
-    const { rerender } = render(<GenreItem item={genre} />);
+    const { rerenderGenreItem } = renderGenreItem();
 
-    rerender(<GenreItem item={genre} isSelected={true} />);
+    rerenderGenreItem({ isSelected: true });
 
     let genreItem = screen.getByText(genre);
     expect(genreItem).toContainHTML('<span class="arrow"></span>');
 
-    rerender(<GenreItem item={genre} isSelected={false} />);
+    rerenderGenreItem({ isSelected: false });
 
     genreItem = screen.getByText(genre);
     expect(genreItem).not.toContainHTML('<span class="arrow"></span>');
   });
 
   it('renders and rerenders as active and back', () => {
-    const { rerender } = render(<GenreItem item={genre} />);
+    const { rerenderGenreItem } = renderGenreItem();
 
-    rerender(<GenreItem item={genre} isActive={true} isSelected={true} />);
+    rerenderGenreItem({ isActive: true, isSelected: true });
 
     let genreItem = screen.getByText(genre);
     expect(genreItem).toHaveClass('genre-item-active');
     expect(genreItem).toContainHTML('<span class="arrow"></span>');
 
-    rerender(<GenreItem item={genre} isActive={false} isSelected={false} />);
+    rerenderGenreItem({ isActive: false, isSelected: false });
 
     genreItem = screen.getByText(genre);
     expect(genreItem).not.toHaveClass('genre-item-active');
@@ -48,9 +60,9 @@ describe('GenreItem', () => {
   });
 
   it('renders as active and rerenders as selected', () => {
-    const { rerender } = render(<GenreItem item={genre} isActive={true} isSelected={true} />);
+    const { rerenderGenreItem } = renderGenreItem({ isActive: true, isSelected: true });
 
-    rerender(<GenreItem item={genre} isActive={false} isSelected={true} />);
+    rerenderGenreItem({ isActive: false, isSelected: true });
 
     let genreItem = screen.getByText(genre);
     expect(genreItem).not.toHaveClass('genre-item-active');
